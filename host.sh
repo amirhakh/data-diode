945eb908b973f8ea2220b5426eaed0ef51b6e658
amir <amir@civil.ba>
2019-03-30 16:04:43 +0430

    add first release

---
+++ host.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# add crontab task to disable/enable interface | delete file
+# add logrotate
+# add permission
+# add user
+# harden
+# schedule for active houre
+# log per user
+# log for viruse | executable | connection | login | ...
+
+# apt install docker.io docker-compose
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose build
+docker-compose up -d
+
+crontab -l | { cat ; echo -e "0 8 * * 6 docker-compose -f /$(pwd)/docker-compose.yml start\n0 20 * * 6 docker-compose -f /$(pwd)/docker-compose.yml stop" ; } | crontab -
+crontab -l | { cat ; echo -e "0 8 * * * test $(date +'%a') -ne 5 && docker-compose -f /$(pwd)/docker-compose.yml start" ; } | crontab -
+crontab -l | { cat ; echo -e "0 20 * * * test $(date +'%a') -ne 5 && docker-compose -f /$(pwd)/docker-compose.yml stop" ; } | crontab -
+
---
+++ output/service.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+function cleanup {
+  echo "exiting"
+  umount /mnt/migrate
+}
+trap cleanup EXIT
+trap cleanup INT
+
+echo "mount -t cifs -o rw,username=$user,password=$password //$smbip/$smbpath /mnt/migrate"
+mount -t cifs -o rw,username=$user,password=$password //$smbip/$smbpath /mnt/migrate
+
+while true
+do
+    sleep 5
+    #| logger --id migrate -f
+    o=$(rsync -Wav --stats --delete /migrate/ /mnt/migrate/) #| tee -a /var/log/migrate.log
+    ol=$(echo "$o" | wc -l)
+    test $ol -gt 18 && echo "$o"
+done
